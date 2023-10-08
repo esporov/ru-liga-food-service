5a555cf8aa0fae5c2d339688c482167eba9d683b
esporov <egorsporov@yandex.ru>
2023-10-08 20:35:18 +0300

    feat: add notification service

---
+++ notification-service/src/main/java/ru/liga/NotificationServiceApp.java
@@ -0,0 +1,12 @@
+package ru.liga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApp.class, args);
+    }
+}
