39db3704ddf97980df042b70c128b0c46c08c8d4
esporov <egorsporov@yandex.ru>
2023-10-16 04:44:36 +0300

    поправил репозиторий/сервис/контроллер ресторана

--- kitchen-service/src/main/java/ru/liga/repository/RestaurantRepository.java
+++ kitchen-service/src/main/java/ru/liga/repository/RestaurantRepository.java
@@ -1,22 +1,23 @@
 package ru.liga.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.liga.domain.restaurant.Restaurant;
+import ru.liga.domain.restaurant.RestaurantAddress;
+import ru.liga.domain.restaurant.RestaurantStatus;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-    Optional<Restaurant> findByRestaurantName(String restaurantName);
+    Optional<Restaurant> getRestaurantByRestaurantId(long restaurantId);
 
-    @Query(value = "SELECT * " +
-            "FROM restaurants " +
-            "WHERE restaurant_id = :id", nativeQuery = true)
-    Optional<Restaurant> getRestaurantById(@Param("id") long id);
+    @Query(value = "select ra from RestaurantAddress ra where ra.restaurant.restaurantId = :id")
+    List<RestaurantAddress> getAllRestaurantsByRestaurantId(@Param("id") long id);
 
     @Query(value = "SELECT * FROM restaurants " +
             "LEFT JOIN restaurants_restaurant_address USING (restaurant_id) " +
@@ -24,23 +25,27 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
             "WHERE restaurant_name = :restaurantName", nativeQuery = true)
     List<Restaurant> findAllByRestaurantName(@Param("restaurantName") String restaurantName);
 
-    @Query(value = "UPDATE restaurants " +
-            "SET restaurant_name = : newRestaurantname " +
-            "WHERE restaurant_name = : restaurantname", nativeQuery = true)
-    void updateRestaurantName(@Param("restaurantName") String restaurantName,
-                              @Param("newRestaurantName") String newRestaurantName);
+    @Modifying
+    @Query(value = "update RestaurantAddress ra  set ra.restaurantStatus = :restaurantStatus where ra.addressId = :addressId")
+    void updateRestaurantStatusByAddressId(@Param("addressId") long addressId,
+                                           @Param("restaurantStatus") String restaurantStatus);
+
+    @Query(value =
+            "select ra " +
+                    "from RestaurantAddress ra " +
+                    /*"left join fetch ra.restaurant r  " +*/
+                    "where ra.restaurantStatus = :restaurantStatus")
+    List<RestaurantAddress> getRestaurantByRestaurantStatus(@Param("restaurantStatus") RestaurantStatus restaurantStatus);
 
 
-    @Query(value = "UPDATE restaurant_address " +
+    void updateByRestaurantName(String restaurantName, String newRestaurantName);
+
+
+
+    /*@Query(value = "UPDATE restaurant_address " +
             "SET restaurant_status = :restaurantStatus " +
-            "WHERE  address_id IN " +
-            "(SELECT address_id " +
-            "FROM restaurants " +
-            "LEFT JOIN restaurants_restaurant_address USING (restaurant_id) " +
-            "LEFT JOIN restaurant_address USING (address_id) " +
-            "WHERE restaurant_name = :restaurantName AND address_id = :addressId)", nativeQuery = true)
-    void updateRestaurantStatusByRestaurantNameAndAddressId(@Param("restaurantName") String restaurantName,
-                                                            @Param("addressId") long addressId,
-                                                            @Param("restaurantStatus") String restaurantStatus);
+            "WHERE  address_id = :addressId", nativeQuery = true)
+    void updateRestaurantStatusByAddressId(@Param("addressId") long addressId,
+                                           @Param("restaurantStatus") String restaurantStatus);*/
 
 }
--- kitchen-service/src/main/java/ru/liga/service/RestaurantService.java
+++ kitchen-service/src/main/java/ru/liga/service/RestaurantService.java
@@ -2,26 +2,18 @@ package ru.liga.service;
 
 import ru.liga.domain.restaurant.Restaurant;
 import ru.liga.domain.exception.RestaurantNotFoundException;
+import ru.liga.domain.restaurant.RestaurantAddress;
 
 import java.util.List;
 
 public interface RestaurantService {
 
-    Restaurant getRestaurantById(long id) throws RestaurantNotFoundException;
-
-//    Restaurant getRestaurantByName(String Name) throws RestaurantNotFoundException;
-
+    Restaurant getRestaurantByRestaurantId(long id) throws RestaurantNotFoundException;
+    List<RestaurantAddress> getAllRestaurantsByRestaurantId(long id) throws RestaurantNotFoundException;
     List<Restaurant> getAllDistinctRestaurants();
-
     List<Restaurant> getAllRestaurantsByRestaurantName(String name);
-
     void updateRestaurantName(String restaurantName, String newRestaurantName);
-
-    void updateRestaurantStatusByRestaurantNameAndAddressId(String restaurantName, long addressId, String restaurantStatus);
-
+    void updateRestaurantStatusByAddressId(long addressId, String restaurantStatus);
+    public List<RestaurantAddress> getRestaurantByRestaurantStatus(String restaurantStatus);
     void createRestaurant(Restaurant restaurant);
-
-
-
-
 }
